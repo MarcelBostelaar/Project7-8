b796260af0f762725af145cc203b2bfa73346a27
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2017-06-29 23:18:51 +0200

    added data generator

--- Database_API/Database_API/Database_API/database.py
+++ Database_API/Database_API/Database_API/database.py
@@ -5,86 +5,96 @@ import os
 
 
 class Database(object):
-    # Contructior opens database
-    def __init__(self, databasePath = "Project78.db"):
-        self.databasePath = databasePath
-        self.select = ""
-        self.froms = ""
+	# Contructior opens database
+	def __init__(self, databasePath = "Project78.db"):
+		self.databasePath = databasePath
+		self.select = ""
+		self.froms = ""
 
-    def open_connection(self):
-        self.connect = sqlite3.connect("Project78.db")
+	def open_connection(self):
+		self.connect = sqlite3.connect("Project78.db")
 
-        # this file test use route
-        self.cursor = self.connect.cursor()
-        self.connect.row_factory = sqlite3.Row
+		# this file test use route
+		self.cursor = self.connect.cursor()
+		self.connect.row_factory = sqlite3.Row
 
-    def close_connetion(self):
-        try:
-            self.connect.commit()
-        except:
-            print("Could not commit")
-        self.connect.close()
+	def close_connetion(self):
+		try:
+			self.connect.commit()
+		except:
+			print("Could not commit")
+		self.connect.close()
 
-    # executes given query
-    # returns number of rows affected by query or last rowid
-    def raw_query(self, query, rowcount = True):
-        try:
-            self.open_connection()
-            self.cursor.execute(query)
-            if rowcount:
-                result = self.cursor.rowcount
-                print(result)
-            else:
-                result = self.cursor.lastrowid
-                print(result)
-            self.close_connetion()
-        except:
-            result = sys.exc_info()
-            print(result)
-            print("raw query error")
-            print(query)
-        return result
+	# executes given query
+	# returns number of rows affected by query or last rowid
+	def raw_query(self, query, rowcount = True):
+		try:
+			self.open_connection()
+			self.cursor.execute(query)
+			if rowcount:
+				result = self.cursor.rowcount
+				print(result)
+			else:
+				result = self.cursor.lastrowid
+				print(result)
+			self.close_connetion()
+		except:
+			result = sys.exc_info()
+			print(result)
+			print("raw query error")
+			print(query)
+		return result
 
-    #Get all kantinedata
-    def getkantinedata(self):
-        self.open_connection()
-        try:
-            self.cursor.execute("select * from kantinedata")
-            result = self.cursor.fetchall()
-            self.close_connetion()
-            return result
-        except:
-            pass
-            self.close_connection()
-        
-        
-    def postkantinedata(self, id, timein, timeout, date):
-        self.open_connection()
-        try:
-            self.cursor.execute("INSERT INTO kantinedata(id, timein, timeout, date) Values({0}, '{1}', '{2}', '{3}')".format(id, timein, timeout, date))
-            self.close_connetion()
-            return 200
-        except:
-            result = sys.exc_info()
-            print(result)
-            self.close_connetion()
-            return 400
+	def resetdatabase(self):
+		try:
+			self.open_connection()
+			result = self.cursor.execute("drop table kantinedata")
+			self.cursor.execute("CREATE TABLE `Kantinedata` (`ID`	string,`timein`	time,`timeout`	time,`date`	date,`vakantiedag`	INTEGER,`temperatuur`	INTEGER,`regen`	INTEGEr);")
+			print(result)
+		except:
+			result = sys.exc_info()
+			print(result)
+			self.close_connetion()
+	#Get all kantinedata
+	def getkantinedata(self):
+		self.open_connection()
+		try:
+			self.cursor.execute("select * from kantinedata")
+			result = self.cursor.fetchall()
+			self.close_connetion()
+			return result
+		except:
+			pass
+			self.close_connection()
+		
+		
+	def postkantinedata(self, id, timein, timeout, date, vakantiedag, temperatuur, regen):
+		self.open_connection()
+		try:
+			self.cursor.execute("INSERT INTO kantinedata(id, timein, timeout, date, vakantiedag, temperatuur, regen) Values({0}, '{1}', '{2}', '{3}', {4}, {5}, {6})".format(id, timein, timeout, date, vakantiedag, temperatuur, regen))
+			self.close_connetion()
+			return 200
+		except:
+			result = sys.exc_info()
+			print(result)
+			self.close_connetion()
+			return 400
 
-    #Get function from raw query
-    #Example: <database>.getraw("select * from testertable")
-    def getrawquery(self, query):
-        self.open_connection()
-        try:
-            self.cursor.execute(query)
-            result = self.cursor.fetchall()
-            print(result)
-            return result
-        except:
-            result = sys.exc_info()
-            print(result)
-            print("Get query error. \n", query)
-            return result
-        self.close_connetion()
+	#Get function from raw query
+	#Example: <database>.getraw("select * from testertable")
+	def getrawquery(self, query):
+		self.open_connection()
+		try:
+			self.cursor.execute(query)
+			result = self.cursor.fetchall()
+			print(result)
+			return result
+		except:
+			result = sys.exc_info()
+			print(result)
+			print("Get query error. \n", query)
+			return result
+		self.close_connetion()
 
 
 
--- Database_API/Database_API/Database_API/generatedata.py
+++ Database_API/Database_API/Database_API/generatedata.py
@@ -2,34 +2,105 @@ from Database_API import database
 import random
 
 def GenerateData():
-    db = database.Database()
-    #i in range(start, end) (0,500) makes 500 cycles for data generation
-    for i in range(0,5000):
-        #generate random data
-        id = random.randrange(0,200)
-        timein = generatetime()
-        timeout = generatetime()
+	db = database.Database()
+	#i in range(start, end) (0,500) makes 500 cycles for data generation
+	for i in range(0,200):
+		wheatherdata = generatedate()
+		print("test1")
+		if wheatherdata['vakantiedag'] == 1 and wheatherdata['temperature'] > 19 and wheatherdata['regen'] == 0:
+			#very low amounts of people in kantine
+			tmpint = random.randrange(11, 34)
+			for i in range(1, tmpint):
+				initials = generateInitials()
+				db.postkantinedata(initials['id'], initials['timein'], initials['timeout'], wheatherdata['date'], wheatherdata['vakantiedag'], wheatherdata['temperature'], wheatherdata['regen'])
+		if wheatherdata['vakantiedag'] == 0 and wheatherdata['temperature'] < 18 and wheatherdata['regen'] == 1:
+			#very high amounts of people in kantine
+			tmpint = random.randrange(32, 78)
+			for i in range(1, tmpint):
+				initials = generateInitials()
+				db.postkantinedata(initials['id'], initials['timein'], initials['timeout'], wheatherdata['date'], wheatherdata['vakantiedag'], wheatherdata['temperature'], wheatherdata['regen'])
+		if wheatherdata['vakantiedag'] == 0 and wheatherdata['temperature'] > 16 and wheatherdata['regen'] == 0:
+			#very high amounts of people in kantine
+			tmpint = random.randrange(14, 38)
+			for i in range(1, tmpint):
+				initials = generateInitials()
+				db.postkantinedata(initials['id'], initials['timein'], initials['timeout'], wheatherdata['date'], wheatherdata['vakantiedag'], wheatherdata['temperature'], wheatherdata['regen'])
+		if wheatherdata['vakantiedag'] == 1 and wheatherdata['temperature'] < 18 and wheatherdata['regen'] == 0:
+			#very high amounts of people in kantine
+			tmpint = random.randrange(23, 64)
+			for i in range(1, tmpint):
+				initials = generateInitials()
+				db.postkantinedata(initials['id'], initials['timein'], initials['timeout'], wheatherdata['date'], wheatherdata['vakantiedag'], wheatherdata['temperature'], wheatherdata['regen'])
+		else:
+			tmpint = random.randrange(30, 65)
+			for i in range(1, tmpint):
+				initials = generateInitials()
+				db.postkantinedata(initials['id'], initials['timein'], initials['timeout'], wheatherdata['date'], wheatherdata['vakantiedag'], wheatherdata['temperature'], wheatherdata['regen'])
+
+def generateInitials():
+	id = random.randrange(0,200)
+	generatetimes = generatetime()
+	timein = generatetimes['timein']
+	timeout = generatetimes['timeout']
+	return {'id':id, 'timein':timein, 'timeout':timeout}
 
 def generatetime():
-    hour = random.randrange(0,23)
-    minute = random.randrange(0,59)
-    second = random.randrange(0,59)
-    time1 = "{0}:{1}:{2}".format(hour,minute,second)
-    return time1, time2
+	hour = random.randrange(11,14)
+	minute = random.randrange(0,59)
+	second = random.randrange(0,59)
+	#timein == time when the client walks in for lunch
+	#time2 == time the client takes for lunching
+	#timeout == time when the client leaves the lunch
+	timein = "{0}:{1}:{2}".format(hour,minute,second)
+	time2 = random.randrange(13,51) #13 to 51 minutes
+	if time2 + minute >= 60:
+		minute = time2 + minute - 60
+		hour = hour + 1
+	else:
+		minute = time2 + minute
+	timeout = "{0}:{1}:{2}".format(hour,minute,second)
+	return {'timein':timein, 'timeout':timeout}
 
 def generatedate():
-    year = 2017
-    month = random.randrange(0,6)
-    if month == 1 or month == 3 or month == 5:
-        day = random.randrange(0,31)
-    elif month == 2:
-        day = random.randrange(0,28)
-    else:
-        day = random.randrange(0,30)
-    date = "{0}:{1}:{2}".format(year,month,day)
-    return date
+	#defining
+	year = 2017
+	month = random.randrange(1,6)
+	day = 0
+	temp = 0
+	regen = 0
+	#creating month
+	if month == 1 or month == 3:
+		day = random.randrange(0,31)
+		temp = random.randrange(0, 15)
+	elif month == 2:
+		day = random.randrange(0,28)
+		temp = random.randrange(0,11)
+	elif month == 5:
+		day = random.randrange(0,31)
+		temp = random.randrange(13,28)
+	elif month == 4:
+		day = random.randrange(0,30)
+		temp = random.randrange(7, 17)
+	else:
+		day = random.randrange(0,30)
+		temp = random.randrange(16,35)
+	#chance of 1 out of 20 for a vakantiedag
+	vakantiedag = random.randrange(1,20)
+	if vakantiedag == 19:
+		vakantiedag = 1
+	else:
+		vakantiedag = 0
+	#chance of about 1 of 6 for rain
+	regen = random.randrange(1,6)
+	if regen == 5:
+		regen = 1
+	else:
+		regen = 0
+	date = "{0}:{1}:{2}".format(year,month,day)
+	print(date ,'\n', vakantiedag, regen, temp)
+	return {'date':date, 'temperature': temp, 'vakantiedag':vakantiedag, 'regen':regen}
 
 
-
+GenerateData()
 
 
