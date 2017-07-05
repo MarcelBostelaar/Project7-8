8657bf080560716d415779c7d1f25e59a5676d38
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2017-07-05 16:13:29 +0200

    Made new dummy data generating code

---
+++ Database_API/Database_API/Database_API/NewGenerateData.py
@@ -0,0 +1,46 @@
+import math
+import random
+
+class entry():
+
+    def __init__(self, rain, vacation, timein, timeout, date):
+        self.Rain = rain
+        self.Vacation = vacation
+        self.TimeIn = timein
+        self.TimeOut = timeout
+        self.Date = date
+        self.Temprature = 21
+
+def generateDataNew():
+    alle_entries = []
+    for maand in range(1, 13):
+        for dag in range(1,29):
+            rain = random.randint(0,1)
+            vacation = random.randint(0,1)
+            for uur in range(1, 24):
+                for kwartier in range(1,5):
+                    amountOfPeople = (int)(GetBaseAmount(rain, vacation) * GetTimeMultiplier(uur, kwartier*15) * GetDeviationMultiplier())
+                    timein = (str)(uur) + ":" + (str)(kwartier * 15 - 10)
+                    timeout = (str)(uur) + ":" + (str)(kwartier * 15 - 5)
+                    date = "2017:" + (str)(maand) + ":" + (str)(dag)
+                    for people in range(0,amountOfPeople):
+                        alle_entries.append(entry(rain, vacation, timein, timeout, date))
+    return alle_entries
+    
+
+    
+def GetTimeMultiplier(Hours, Minutes):
+    totaltime = Hours*60 + Minutes
+    totaltime=totaltime / 60
+    result = math.pow(math.e, (-0.5)*(totaltime-12)*(totaltime-12))/ math.sqrt(2*math.pi) * (1/0.4)
+    return result
+
+def GetDeviationMultiplier():
+    randomfloat = random.randint(-1000, 1000) / 2000
+    return math.pow(math.e, (-0.5)*(randomfloat*3)*(randomfloat*3))/ math.sqrt(2*math.pi) * (1/0.4) * -1 + 1.5
+
+def GetBaseAmount(rain, vacation):
+    table = [[40,10],[60,20]]
+    return table[rain][vacation]
+
+result = generateDataNew()
